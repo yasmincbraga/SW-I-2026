b4de5e2a05ce044c17dd7fb5efe0bc0a3973374d
Yasmin <yasmincarvalhobraga2@gmail.com>
2026-03-05 21:48:54 -0300

    Add files via upload

--- aula04/exercicio03.php
+++ aula04/exercicio03.php
@@ -7,5 +7,21 @@
     $v2 = 45;
 
     $soma = $v1 + $v2;
-    echo "Soma "
+    echo "Soma: $soma ";
+    echo "<hr>";
+
+    $sub = $v1 - $v2;
+    echo "Subtração: $sub";
+    echo "<hr>";
+
+    $mult = $v1 * $v2;
+    echo "Multiplicação: $mult";
+    echo "<hr>";
+
+    $div = $v1 / $v2;
+    echo "Divisão: $div";
+    echo "<hr>";
+
+    $resto = $v1 % $v2;
+    echo "Resto da divisão: $resto";
 ?>
