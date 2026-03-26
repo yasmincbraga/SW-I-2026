6c87a564462964089a73fc275609e204a62bbea9
Yasmin <yasmincarvalhobraga2@gmail.com>
2026-03-26 12:19:51 -0300

    Add files via upload

---
+++ aula06/restrita.php
@@ -0,0 +1,15 @@
+<?php
+    $nome = $_GET['nome'];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ÁREA RESTRITA</title>
+</head>
+<body>
+    <p>Bem vindo <?php echo $nome; ?> a sua área restrita!</p>
+</body>
+</html>
